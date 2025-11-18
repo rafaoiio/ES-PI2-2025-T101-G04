0fb3a54b25bd50f6d74de9c933f491f6ea3ee2ff
Laura Soares <laura.csoares24@gmail.com>
2025-11-17 21:54:37 -0300

    Estrutura base do sistema de autenticação
    Configuração inicial do módulo de autenticação com estratégias JWT e Local.

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,3 +1,4 @@
+// LAURA
 //Autor Rafael Gaudencio Dias
 // Descrição: Valida o token JWT e libera acesso apenas a usuários autenticados.
 import { Injectable } from '@nestjs/common';
@@ -5,13 +6,12 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy)
 // Crio a classe que defino como o token será validado
- {
+export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       secretOrKey: process.env.JWT_SECRET!,
-     // uso a chave secreta o .env para validar o token
+      // uso a chave secreta o .env para validar o token
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       // digo que o token vem do Header
     });
@@ -20,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy)
   async validate(payload: any) {
     // função sendo executada depois que o token é validado
     // dados contidos dentro do token { sub, email, name, iat, exp }
-    return { userId: payload.sub, email: payload.email, name: payload.nome };
+    return { userId: payload.sub, email: payload.email, name: payload.name };
     // retorno as informações do usuário para o request, e deixo acessível
   }
 }
